1db17ffd2a0fb7199bf69a5c620f84c2b515387a
Poden <56049210+Poden@users.noreply.github.com>
2020-01-31 16:00:45 -0600

    update

--- src/components/CartForm.js
+++ src/components/CartForm.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function Cart (){
+function CartForm (){
 
 
 }
 
-export default Cart;
+export default CartForm;
--- src/components/TotalForm.js
+++ src/components/TotalForm.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function Total(){
+function TotalForm(){
 
 
 }
 
-exxport default Total;
+export default TotalForm;
